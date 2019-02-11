a0803beb31afd679d5f72e7c357ed8c06cb46ae8
dmnkdm <dmnkdm@yandex.ru>
2019-02-12 00:28:47 +0500

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index-bundle.js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+             {
+                 test: /\.(png|svg|jpg|gif)$/,
+                 use: [
+                   'file-loader'
+                 ]
+            }
+           
+        ]
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+  ]
+};
